13b4c8823826584bb94927f8199117ecb2d85dbd
Xavier Trinidad <xmtrinidad2@gmail.com>
2018-12-06 06:15:40 -0700

    Created separate routing for section 5

--- Section-5/app.js
+++ Section-5/app.js
@@ -1,21 +1,19 @@
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
-});
+// Routes
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>');
-});
+
 
 app.listen(3000);
