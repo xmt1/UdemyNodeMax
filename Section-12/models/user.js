b9e24454ecc9d99011b045db3a272272d0530431
Xavier Trinidad <xmtrinidad2@gmail.com>
2019-06-17 18:28:21 -0700

    updated for section 14

---
+++ Section-12/models/user.js
@@ -0,0 +1,59 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+  
+  constructor(username, email, cart, id) {
+    this.username = username;
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId == product._id;
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({productId: new ObjectId(product._id), quantity: 1});
+    }
+
+    
+    
+    const updatedCart = {items: updatedCartItems};
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new ObjectId(this._id)},
+        { $set: {cart: updatedCart} }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({_id: new ObjectId(userId)})
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => console.log(err));
+  }
+
+}
+
+module.exports = User;
